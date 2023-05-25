f206bd4b3fb5fda9910dbaeafa02d3fd7f2cb2f4
SJ Jeon <ayclub8866@naver.com>
2023-05-25 22:35:25 +0900

    update: Header fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Header.css';
 
 function Header() {
-    return(
-    <header>
-        <div className="header-left">jsj.dev</div>
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const topOffset = window.pageYOffset;
+      if (topOffset > 0) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className={isSticky ? 'header sticky' : 'header'}>
+      <a href="#" className="header-left">jsj.dev</a>
       <nav>
-        <ul>
+        <ul className = "header-ul">
           <li><a href="#about">About</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#skills">Skills</a></li>
