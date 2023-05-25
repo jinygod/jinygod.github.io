1e532e52ceafb5364f9118cdaa3985a2596bf943
SJ Jeon <ayclub8866@naver.com>
2023-05-25 22:54:09 +0900

    update: create components' grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import MainSection from './components/MainSection';
+import Project from './components/Project';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <MainSection />
+    <div className="container">
+      <Header className="header" />
+      <MainSection className="main-section" />
+      <Project className="project" />
     </div>
   );
 }
---
+++ src/components/Project.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Project.css';
+
+function Projects() {
+  return (
+    <section className="projects-section">
+      <div className="timeline">
+        {/* Timeline items */}
+        <div className="timeline-item">
+          <div className="timeline-item-content">
+            <div className="project-media">
+              {/* Project media (photo, video, etc.) */}
+            </div>
+            <div className="project-description">
+              {/* Project description */}
+            </div>
+          </div>
+        </div>
+        {/* Add more timeline items */}
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
