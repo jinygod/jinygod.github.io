c1319b3ed2bd169bd6c102ad5967156b3fada2e4
JeonSeongJin <ayclub8866@naver.com>
2023-05-30 17:20:19 +0900

    update: new Project

--- src/components/projectSection/ProjectSection.js
+++ src/components/projectSection/ProjectSection.js
@@ -40,12 +40,12 @@ function ProjectSection() {
           <a href = "https://github.com/jinygod/peonyshop">GitHub Link</a>
 </div>
         </li>
-{/*         <li style={{ "--accent-color": "#E24A68" }}>
+        <li style={{ "--accent-color": "#E24A68" }}>
           <div className="date">2021</div>
           <div className="title">PeonyFactory</div>
           <div className="descr">고객의 주문을 받아 관리자가 승인하고 처리 / 판매량을 모니터링하는 사이트</div> 
          </li>
-        <li style={{ "--accent-color": "#1B5F8C" }}>
+  {/*       <li style={{ "--accent-color": "#1B5F8C" }}>
          <div className="date">2021</div>
           <div className="title">PeonyFactory</div>
           <div className="descr">고객의 주문을 받아 관리자가 승인하고 처리 / 판매량을 모니터링하는 사이트</div>
